7c93393af35eb0be983c6d859bee878d8a97f796
Pavithraramesh19 <pavithraramesh1902@gmail.com>
2024-02-16 10:25:58 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConxoleAppfeb16;
+
+Console.WriteLine("Welcome to Number System");
+Console.WriteLine("***Even Number***");
+Class1.EvenNum();
+Console.WriteLine("***Odd Number***");
+Class1.OddNum();
